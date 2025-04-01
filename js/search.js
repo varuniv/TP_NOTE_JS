10f46ea51b7350144c5bd070706135c7b84e214f
Toniomp <delahayetonio77@gmail.com>
2025-04-01 11:03:45 +0200

    navbar et résultats de recherche disparaisse

--- js/search.js
+++ js/search.js
@@ -1,11 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
     var laRecherche = document.getElementById("search");
-    var divResultat = document.getElementById("results-table").querySelector("tbody");
-    
+    var divResultat = document.getElementById("results-table").querySelector("tbody");  
     laRecherche.addEventListener("input", function () {
         let v = laRecherche.value.toLowerCase();
-           divResultat.innerHTML = ""; 
+        if (v === "") {
+            divResultat.innerHTML = ""; 
+            return;
+        }
         let f = app.characters.filter(c => c.soul.name.toLowerCase().search(new RegExp(v, "i")) != -1);
+        divResultat.innerHTML = "";
         for (let i = 0; i < f.length; i++) {
             let x = document.createElement("tr");
             x.innerHTML = "<td>" + f[i].soul.name + "</td>" + "<td><button class='btn btn-primary' onclick='app.showCharacterDetail(" + f[i].soul.id + ")'>Voir</button></td>";
@@ -13,4 +16,3 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
-    
